a61764b42dea59c8acf8d3ba3e3125a9310e6438
Tara Tan <Tara.Tan@copadata.com>
2025-11-10 14:29:35 +0100

    Stage 1: Basic calculator with if/else

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        //// Stack erstellen
+        Stack<int> mystackname = new Stack<int>();
+        mystackname.Push(100);
+        mystackname.Push(22);
+        mystackname.Push(34); /// Last in, first out
+        
+        //// Oberste Nummer anzeigen (Peek)
+        Console.WriteLine("Top Number: " + mystackname.Peek()); /// 34
+        
+        /// Element entfernen (Pop)
+        Console.WriteLine("Remove Number: " + mystackname.Pop()); /// 34 removed
+        
+        /// Die restliche Nummern anzeigen
+        Console.WriteLine("Next Number: " + mystackname.Peek()); /// now 22
+        
+        /// Alle Elemente ausgeben
+        Console.WriteLine("Inhalt des Stacks; ");
+        foreach (int number in mystackname)
+            {
+            Console.WriteLine(number);
+            }
+    }
+}
